b586f41979ed0a6de81de8799a019b5b7af0da62
makepost <makepost@firemail.cc>
2018-02-04 02:02:56 +0200

    TypeScript definitions

---
+++ nice-ip.d.ts
@@ -0,0 +1,21 @@
+export function bin(ip: string, prefix?: string);
+
+export function bin6(ip: string, prefix?: string);
+
+export function binPart6(x: string);
+
+export function expand6(ip: string);
+
+export function bin4(ip: string, prefix?: string);
+
+export function binPart4(x: string);
+
+export function nice(bin: string);
+
+export function nice6(bin: string);
+
+export function nicePart6(bin: string);
+
+export function nice4(bin: string);
+
+export function nicePart4(bin: string);
