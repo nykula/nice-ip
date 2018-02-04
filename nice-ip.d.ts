3fd01aec763d8901646837788bbe139ddd481172
makepost <makepost@firemail.cc>
2018-02-04 02:21:47 +0200

    Annotate return types

--- nice-ip.d.ts
+++ nice-ip.d.ts
@@ -1,21 +1,21 @@
-export function bin(ip: string, prefix?: string);
+export function bin(ip: string, prefix?: string): string;
 
-export function bin6(ip: string, prefix?: string);
+export function bin6(ip: string, prefix?: string): string;
 
-export function binPart6(x: string);
+export function binPart6(x: string): string;
 
-export function expand6(ip: string);
+export function expand6(ip: string): string;
 
-export function bin4(ip: string, prefix?: string);
+export function bin4(ip: string, prefix?: string): string;
 
-export function binPart4(x: string);
+export function binPart4(x: string): string;
 
-export function nice(bin: string);
+export function nice(bin: string): string;
 
-export function nice6(bin: string);
+export function nice6(bin: string): string;
 
-export function nicePart6(bin: string);
+export function nicePart6(bin: string): string;
 
-export function nice4(bin: string);
+export function nice4(bin: string): string;
 
-export function nicePart4(bin: string);
+export function nicePart4(bin: string): string;
